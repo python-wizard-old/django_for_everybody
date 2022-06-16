1e3b806f0216d20ab2ea3b74c499e618f6b9eb16
Python Wizard <max.python.wizard@gmail.com>
2022-06-16 05:44:19 +0200

    after owner view to the views.py and owner in urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('owner', views.owner, name='owner'),
 ]
