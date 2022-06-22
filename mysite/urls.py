1503719a4e0bef1de1e8d398fa19e1ecd459cc16
Python Wizard <max.python.wizard@gmail.com>
2022-06-23 01:08:26 +0200

    after course 1, week 1, checkpoint 1

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
 from django.views.static import serve
+from django.views.generic import TemplateView
 
 # Up two folders to serve "site" content
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -16,6 +17,7 @@ SITE_ROOT = os.path.join(BASE_DIR, 'site')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('', TemplateView.as_view(template_name='home/main.html')),
     url(r'^site/(?P<path>.*)$', serve,
         {'document_root': SITE_ROOT, 'show_indexes': True},
         name='site_path'
