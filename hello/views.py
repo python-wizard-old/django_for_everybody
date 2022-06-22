17b9ae84c8ddc7042d9405d2fb84f230b957789e
Python Wizard <max.python.wizard@gmail.com>
2022-06-23 01:40:34 +0200

    after course 3, week 1, checkpoint 3 sessions

--- hello/views.py
+++ hello/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello_view():
-	return HttpResponse("Hello, world. 0b6cf75f is the polls owner.")
+def hello_view(request):
+	num_visits = request.session.get('num_visits', 0) + 1
+	request.session['num_visits'] = num_visits
+
+	if num_visits > 4:
+		del (request.session['num_visits'])
+
+	return HttpResponse('View count = ' + str(num_visits))
